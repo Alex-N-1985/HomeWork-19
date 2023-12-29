740acef603705d19979c3838edfe845b594bdf2a
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2023-12-29 23:01:01 +0300

    first commit.

---
+++ scripts.js
@@ -0,0 +1,25 @@
+/* Задание №1*/
+
+let pet1 = {
+    name : "Пиксель",
+    type : "Кот",
+    sound: "Мяу"
+},
+    pet2 = {
+    name: "Бакс",
+    type: "Пес",
+    sound: "Гав"
+};
+
+ pet1.say = function() {
+    let output = this.type + " " + this.name +  " говорит " + this.sound;
+    return output;
+ };
+
+ pet2.say = function() {
+    let output = this.type + " " + this.name +  " говорит " + this.sound;
+    return output;
+ };
+
+console.log(pet1.say());
+console.log(pet2.say());
